aef161580cd854d5be895dce847a364bf3f7189e
Sophie <socarboni@gmail.com>
2021-12-14 17:35:16 +0100

    only a weird bug with simple quote to manage...

--- srcs/datas/parse_char_on_line.c
+++ srcs/datas/parse_char_on_line.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 09:42:12 by saray             #+#    #+#             */
-/*   Updated: 2021/12/14 16:51:51 by scarboni         ###   ########.fr       */
+/*   Updated: 2021/12/14 17:33:11 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -191,6 +191,7 @@ int		jump_parsing(t_line *line_handle, t_tmp_parsed *tmp_parsed, t_parse_utils p
 
 int	parse_back_slash_inside_quote(char **line, int *i, int quote_id)
 {
+
 	int increment;
 	int delete;
 
@@ -238,71 +239,70 @@ int		parse_back_slash_inside_double_quotes(t_line *line_handle, t_tmp_parsed *tm
 int	find_next__quote(int id_quote, char **line, int i)
 {
 	if (is_sequence_equal_to_parser_code(TYPE_BACK_SLASH, (*line) + i))
-		if (parse_back_slash_inside_quote(line, &i, id_quote) != EXIT_SUCCESS)
-			return (INCOMPLETE_PATTERN);
+		i++;
 	while ((*line)[i] && !is_sequence_equal_to_parser_code(id_quote, (*line) + i))
 	{
 		if (is_sequence_equal_to_parser_code(TYPE_BACK_SLASH, (*line) + i))
-			if (parse_back_slash_inside_quote(line, &i, id_quote) != EXIT_SUCCESS)
-				return (INCOMPLETE_PATTERN);
+			i++;
 		i++;
 	}
 	if (!is_sequence_equal_to_parser_code(id_quote, (*line) + i))
-				return (INCOMPLETE_PATTERN);
+			return (INCOMPLETE_PATTERN);
 	return (i);
 }
 
-int		parse__quote(t_line *line_handle, t_tmp_parsed *tmp_parsed, t_parse_utils p_utils, int replace)
+int		parse_double_quote(t_line *line_handle, t_tmp_parsed *tmp_parsed, t_parse_utils p_utils)
 {
 	int first_quote;
 	int last_quote;
+	int	ret;
+	int	parse_i;
 	
-
 	(void)tmp_parsed;
 	first_quote = *p_utils.i;
 	ft_strlcpy((*line_handle->line) + first_quote, (*line_handle->line) + first_quote + 1, ft_strlen((*line_handle->line) + first_quote));
 	last_quote = find_next__quote(*p_utils.parse_i, line_handle->line, first_quote);
 	if (last_quote == INCOMPLETE_PATTERN)
 		return (INCOMPLETE_PATTERN);
-	ft_strlcpy((*line_handle->line) + last_quote, (*line_handle->line) + last_quote + 1, ft_strlen((*line_handle->line) + last_quote));
-	if (replace)
+	while (first_quote < last_quote)
 	{
-		// last_quote = replace_fun(line_handle, p_utils, first_quote, last_quote);
-		// if (last_quote == -EXIT_FAILURE)
-		// 	return (last_quote);
+		parse_i = 0;
+		ret = check_parsing(g_parser_dictionary_for_doubles_quotes, *line_handle, *tmp_parsed, (t_parse_utils){p_utils.env, p_utils.i, &parse_i});
+		if (parse_i != DID_NOTHING)
+		{
+			if ((*line_handle->line)[*p_utils.i] == *g_parser_dictionary[TYPE_DOUBLE_QUOTE].code.str)
+			{
+				last_quote = *p_utils.i;
+				break;
+			}
+			last_quote = find_next__quote(*p_utils.parse_i, line_handle->line, (*p_utils.i));
+		}
+		else if (ret != EXIT_SUCCESS)
+			return (ret);
+		(*p_utils.i)++;
 	}
+	ft_strlcpy((*line_handle->line) + last_quote, (*line_handle->line) + last_quote + 1, ft_strlen((*line_handle->line) + last_quote));
 	(*p_utils.i) = last_quote - 1;
 	return (EXIT_SUCCESS);
-}
-
-int		parse_double_quote(t_line *line_handle, t_tmp_parsed *tmp_parsed, t_parse_utils p_utils)
-{
-	return (parse__quote(line_handle, tmp_parsed, p_utils, true));
 }
 
 int	get_rid_of_first_backslash_per_sequence(char **line, int start, int last_quote)
 {
 	int ret;
 
-			printf("RESULTea %d:%d\n",start, last_quote);
 	while (start < last_quote)
 	{
 		ret = ft_strchr_index((*line) + start, *(g_parser_dictionary[TYPE_BACK_SLASH].code.str));
 		if (ret == -EXIT_FAILURE)
 			break;
-			// printf("RESULTe %d\n",ret);
 		start += ret;
-			printf("RESULTea %d\n",start);
 		if (!(*line)[start + 1])
 			break;
-			// printf("break no RESULTea %d\n",((*line) + start + 1));
-		printf("break no RESULTea %d:%s\n",start, (*line) + start);
 		if ((*line)[start + 1] != *(g_parser_dictionary[TYPE_BACK_SLASH].code.str))
 		{
 			start++;
 			continue;
 		}
-			printf("break no RESULTea %d:%s\n",start, (*line) + start);
 		ft_strlcpy((*line) + start, (*line) + start + 1, ft_strlen((*line) + start));
 		last_quote--;
 		start++;
