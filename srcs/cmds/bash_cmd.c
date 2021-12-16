848018340e03806287e03dc645b70339ef8f3cd4
Sophie <socarboni@gmail.com>
2021-12-16 22:12:49 +0100

    path finding for bash commands

--- srcs/cmds/bash_cmd.c
+++ srcs/cmds/bash_cmd.c
@@ -6,16 +6,90 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 09:42:12 by saray             #+#    #+#             */
-/*   Updated: 2021/11/27 12:31:19 by scarboni         ###   ########.fr       */
+/*   Updated: 2021/12/16 22:12:16 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
+#define	MAX_COMPLETES_PATHS 3
+const static	t_str path_completes[MAX_COMPLETES_PATHS] = {
+	(t_str){"/", 1},
+	(t_str){"./", 2},
+	(t_str){"../", 3},
+};
+
+int	is_path_absolute_or_relative(const char *cmd)
+{
+	int	i;
+
+	i = 0;
+	while (i < MAX_COMPLETES_PATHS)
+	{
+		if (ft_strncmp(path_completes[i].str, cmd,
+				path_completes[i].len) == 0)
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+
+char	*get_full_cmd_from_path_int(char *cmd_end_path, char **possibles_paths)
+{
+	char	*cmd_path;
+	int		i;
+
+	i = 0;
+	cmd_path = NULL;
+	while (possibles_paths[i])
+	{
+		if (cmd_path)
+			free(cmd_path);
+		cmd_path = ft_strjoin(possibles_paths[i], cmd_end_path);
+		if (access(cmd_path, F_OK) == 0)
+			return (cmd_path);
+		i++;
+	}
+	if (cmd_path)
+		free(cmd_path);
+	return (NULL);
+}
+
+char	*get_full_cmd_from_path(t_env *env, const char *cmd_name)
+{
+	char	*cmd_path;
+	char	*cmd_end_path;
+
+	cmd_end_path = ft_strjoin("/", cmd_name);
+	if (!cmd_end_path)
+		return (NULL);
+	cmd_path = get_full_cmd_from_path_int(cmd_end_path, env->paths);
+	free(cmd_end_path);
+	return (cmd_path);
+}
+
+char	*get_full_cmd(t_env *env, const char *cmd)
+{
+	if (is_path_absolute_or_relative(cmd) && access(cmd, F_OK) == 0)
+		return (ft_strdup(cmd));
+	if (!env->paths)
+		return (NULL);
+	return (get_full_cmd_from_path(env, cmd));
+}
+
 int		bash_cmd(t_env *env, const char *cmd, const char **args)
 {
-	(void)cmd;
+	char *full_cmd;
+
 	(void)args;
-	(void)env;
+	full_cmd = get_full_cmd(env, cmd);
+	if (!full_cmd)
+	{
+		printf("Command not found: %s\n", cmd);
+		return (EXIT_SUCCESS);
+	}
+	printf("FULL_CMD : %s\n", full_cmd);
+	free(full_cmd);
 	return (EXIT_SUCCESS);
 }
--- srcs/pipex/pipex_stack_exe.c
+++ srcs/pipex/pipex_stack_exe.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 18:54:29 by scarboni          #+#    #+#             */
-/*   Updated: 2021/12/16 17:11:28 by scarboni         ###   ########.fr       */
+/*   Updated: 2021/12/16 20:13:01 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ static void	execute_pipex_stack_int(t_env *env, t_list_double *action)
 	if (!action)
 		return ;
 	current_cell = action->content;
-	// (*fun)(env, *(int *)action->content);
 	select_right_cmd(env, current_cell->args[0], (const char**)current_cell->args);
 	execute_pipex_stack_int(env, action->next);
 }
