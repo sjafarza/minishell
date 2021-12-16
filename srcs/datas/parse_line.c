30f687c6ab08cacfd696457f3adb1c102c46f4ea
Sophie <socarboni@gmail.com>
2021-12-16 17:57:36 +0100

    cleaning a bit

--- srcs/datas/parse_line.c
+++ srcs/datas/parse_line.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 09:42:12 by saray             #+#    #+#             */
-/*   Updated: 2021/12/14 15:50:56 by scarboni         ###   ########.fr       */
+/*   Updated: 2021/12/16 17:57:22 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -140,77 +140,12 @@ int	extract_parsed_groups(t_env *env, char **line)
 		{
 			if (!args || add_back_parsed_groups_stack(env, args, type) == -EXIT_FAILURE)
 				return (-EXIT_FAILURE);
-			printf("AH QUE COUCOU BOB FOR TYPE %d\n", type);
 		}
 		else if(args != NULL)
 		{
 			if (add_back_parsed_groups_stack(env, args, TYPE_CMD) == -EXIT_FAILURE)
 				return (-EXIT_FAILURE);
 		}
-		printf("i is %d:%s:%s\n", i, (*line), (*line)+i);
-		// else if (type != TYPE_CMD) 
-		// {
-		// 	// args = malloc(2 * sizeof(char *));
-		// 	i = go_to_next_needed_i((*line), &is_valid, i);
-		// 	// if (!args)
-		// 	// 	return (-EXIT_FAILURE);
-		// 	tmp = (*line)[i];
-		// 	(*line)[i] = '\0';
-
-		// 	args = init_array_with_one_str(ft_strdup((*line) + start));
-		// 	if (!args)
-		// 		return (-EXIT_FAILURE);
-		// 	// args[0] = ft_strdup((*line) + start);
-		// 	// args[1] = NULL;
-		// 	// if (!args[0])
-		// 	// 	return (-EXIT_FAILURE);
-		// 	add_back_parsed_groups_stack(env, args, type);
-		// 	(*line)[i] = tmp;
-		// }
 	}
 	return (EXIT_SUCCESS);
 }
-
-// int	get_next_args_rec(char *line, int *i, char ***args, int count)
-// {
-// 	int		res;
-// 	char	*arg;
-
-// 	res = extract_next_arg(line, i, &arg);
-// 	if (res != HAS_MORE)
-// 	{
-// 		if (res <= FOUND_ERROR)
-// 			return (res);
-// 		*args = malloc(sizeof(char*) * (count + 2));
-// 		if (!*args)
-// 		{
-// 			free(arg);
-// 			return (FOUND_ERROR);
-// 		}
-// 		(*args)[count + 1] = NULL;
-// 		(*args)[count] = arg;
-// 		return (HAS_NO_MORE);
-// 	}
-// 	res = get_next_args_rec(line, i, args, count + 1);
-// 	if (res <= FOUND_ERROR)
-// 	{
-// 		free(arg);
-// 		return (res);
-// 	}
-// 	(*args)[count] = arg;
-// 	return (HAS_NO_MORE);
-// }
-
-// int	parse_line(t_env *env, char *line)
-// {
-// 	int		i;
-// 	int		res;
-// 	char	**args;
-
-// 	args = NULL;
-// 	i = 0;
-// 	res = get_next_args(line, i, &args);
-// 	while (res == HAS_MORE)
-
-// 	return (EXIT_SUCCESS);
-// }
