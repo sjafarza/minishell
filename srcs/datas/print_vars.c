eb945f56aeb5647a205aa558caf69a83d1d04233
Sophie <socarboni@gmail.com>
2022-01-21 22:43:04 +0100

    want env to be like the real one

--- srcs/datas/print_vars.c
+++ srcs/datas/print_vars.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 09:42:12 by saray             #+#    #+#             */
-/*   Updated: 2021/11/27 21:04:40 by scarboni         ###   ########.fr       */
+/*   Updated: 2022/01/21 22:40:22 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ void	print_vars(t_env *env)
 {
 	int	i;
 
-	printf("Env variables max : %d\n", env->env_vars_max);
-	i = 0;
+	// printf("Env variables max : %d\n", env->env_vars_max);
+	i = 1;
 	while (i < env->env_vars_max)
 	{
-		printf("NAME:[%s] VALUE:[%s]\n", env->env_vars[i].name.str, env->env_vars[i].value.str);
+		printf("%s=%s\n", env->env_vars[i].name.str, env->env_vars[i].value.str);
 		i++;
 	}
 }
