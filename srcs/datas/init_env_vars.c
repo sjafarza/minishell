f66db72c1d1c42324eed6cc0be930826debbbca3
Sophie <socarboni@gmail.com>
2022-01-24 23:21:32 +0100

    quickfix'

--- srcs/datas/init_env_vars.c
+++ srcs/datas/init_env_vars.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 09:42:12 by saray             #+#    #+#             */
-/*   Updated: 2022/01/24 23:08:53 by scarboni         ###   ########.fr       */
+/*   Updated: 2022/01/24 23:20:20 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,14 @@ static int	strings_clean(char *strings[MAX_STRINGS], int code)
 	return (code);
 }
 
-int	init_env_vars_int(t_env *env, char **raw_env, char *strings[MAX_STRINGS],
-	int *i)
+int	init_env_vars(t_env *env, char **raw_env)
 {
-	*i = 0;
+	int		i;
+	int		separator_index;
+	char	*strings[MAX_STRINGS];
+
 	ft_memset(strings, (int) NULL, sizeof(char *) * MAX_STRINGS);
+	i = 0;
 	env->env_vars_max = get_array_size(raw_env);
 	if (env->env_vars_max == NO_ARRAY || env->env_vars_max == 0)
 		return (-EXIT_FAILURE);
@@ -61,24 +64,13 @@ int	init_env_vars_int(t_env *env, char **raw_env, char *strings[MAX_STRINGS],
 	strings[STRING_NAME] = ft_strdup("?");
 	strings[STRING_VALUE] = ft_strdup("0");
 	env->exit_value = 0;
-	env->env_vars[*i] = (t_env_var){(t_str){0}, (t_str){0}, (t_str){0}};
-	if (init_t_str(&env->env_vars[*i].name, strings[STRING_NAME])
+	env->env_vars[i] = (t_env_var){(t_str){0}, (t_str){0}, (t_str){0}};
+	if (init_t_str(&env->env_vars[i].name, strings[STRING_NAME])
 		!= EXIT_SUCCESS
-		|| init_t_str(&env->env_vars[*i].value, strings[STRING_VALUE])
+		|| init_t_str(&env->env_vars[i].value, strings[STRING_VALUE])
 		!= EXIT_SUCCESS)
 		return (strings_clean(strings, -EXIT_FAILURE));
 	i++;
-	return (EXIT_SUCCESS);
-}
-
-int	init_env_vars(t_env *env, char **raw_env)
-{
-	int		i;
-	int		separator_index;
-	char	*strings[MAX_STRINGS];
-
-	if (init_env_vars_int(env, raw_env, strings, &i) != EXIT_SUCCESS)
-		return (-EXIT_FAILURE);
 	while (i < env->env_vars_max)
 	{
 		separator_index = ft_strchr_index(raw_env[i - 1], '=');
