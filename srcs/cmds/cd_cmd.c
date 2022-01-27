e0ed1dbc224c7f63b516b1ebd9b79af1242a4dbd
Sophie <socarboni@gmail.com>
2022-01-27 17:40:02 +0100

    nomre

--- srcs/cmds/cd_cmd.c
+++ srcs/cmds/cd_cmd.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 22:13:42 by saray             #+#    #+#             */
-/*   Updated: 2022/01/25 14:41:34 by scarboni         ###   ########.fr       */
+/*   Updated: 2022/01/27 17:38:55 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,36 +30,47 @@ int	go_back_to_var(t_env *env, const char *var_name_target_folder)
 
 	var = find_env_vars(env, (char *)var_name_target_folder);
 	if (!var)
+	{
+		printf("%s: cd: << %s >> non défini\n", PROMPT_STR,
+			var_name_target_folder);
 		return (-EXIT_FAILURE);
+	}
 	return (go_to(var->value.str));
+}
+
+static int	cd_cmd_leave_early(t_env *env, int args_len, const char *cmd)
+{
+	(void)cmd;
+	if (env->pipex_stack.total_item > 1)
+		return (EXIT_SUCCESS);
+	if (args_len > 2)
+	{
+		printf("%s: cd: trop d'arguments\n", PROMPT_STR);
+		return (1);
+	}
+	if (env->pipex_stack.total_item > 1)
+		return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
 int	cd_cmd(t_env *env, const char *cmd, const char **args)
 {
 	int			args_len;
 
-	(void)cmd;
-	if (env->pipex_stack.total_item > 1)
-		return (EXIT_SUCCESS);
 	args_len = array_len(args);
-	if (args_len > 2)
-	{
-		printf("%s: cd: trop d'arguments\n", PROMPT_STR);
-		return (0);
-	}
-	if (env->pipex_stack.total_item > 1)
-		return (EXIT_SUCCESS);
+	if (cd_cmd_leave_early(env, args_len, cmd) != EXIT_SUCCESS)
+		return (1);
 	if (args_len == 1)
 	{
 		if (go_back_to_var(env, HOME_STR) == -EXIT_FAILURE)
-			return (0);
+			return (1);
 	}
 	else
 	{
 		if (ft_strncmp(args[1], "-", 2) == 0)
 		{
 			if (go_back_to_var(env, OLDPWD_STR) == -EXIT_FAILURE)
-				return (0);
+				return (1);
 		}
 		else if (ft_strlen(args[1]) == 0)
 			return (0);
