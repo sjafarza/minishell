4ae3bdbaa64067281010ec2e5aa582bcb1b4d068
Sophie <socarboni@gmail.com>
2022-01-27 15:34:17 +0100

    norme

--- srcs/cmds/unset_cmd.c
+++ srcs/cmds/unset_cmd.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 16:12:52 by sjafarza          #+#    #+#             */
-/*   Updated: 2022/01/27 15:28:08 by scarboni         ###   ########.fr       */
+/*   Updated: 2022/01/27 15:33:06 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,27 @@ static int	is_valid_as_env_var_name(const char *args)
 	return (false);
 }
 
-int	unset_cmd(t_env *env, const char *cmd, const char **args)
+int	update_path_if_needed(t_env *env, int i, const char **args)
 {
+	if (ft_strncmp(args[i], PATH_STR, PATH_LEN + 1) == 0)
+	{
+		if (env->paths)
+			free_array(env->paths);
+		env->paths = ft_split("", ':');
+		if (!env->paths)
+			return (1);
+	}
+	return (EXIT_SUCCESS);
 }
 
 int	unset_cmd(t_env *env, const char *cmd, const char **args)
 {
 	int	i;
-	int	r;
 	int	error;
 
 	(void)cmd;
 	error = false;
-	if (env->pipex_stack.total_item > 1)
-		return (0);
-	if (!args[1])
+	if (env->pipex_stack.total_item > 1 || !args[1])
 		return (0);
 	i = 0;
 	while (args[++i])
@@ -53,17 +59,9 @@ int	unset_cmd(t_env *env, const char *cmd, const char **args)
 			error = true;
 			continue ;
 		}
-		if ((ft_strlen(args[i]) == PATH_LEN)
-			&& (ft_strncmp(args[i], PATH_STR, PATH_LEN) == 0))
-		{
-			if (env->paths)
-				free_array(env->paths);
-			env->paths = ft_split("", ':');
-			if (!env->paths)
-				return (1);
-		}
-		r = del_env_var(env, (char *)args[i]);
-		if (r != EXIT_SUCCESS)
+		if (update_path_if_needed(env, i, args) != EXIT_SUCCESS)
+			return (1);
+		if (del_env_var(env, (char *)args[i]) != EXIT_SUCCESS)
 			return (1);
 	}
 	return (error);
