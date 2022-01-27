7de02a2d0bb7ceb5a191aa0864a390447ff5b2ae
Sophie <socarboni@gmail.com>
2022-01-27 15:23:09 +0100

    norme

--- srcs/cmds/export_cmd.c
+++ srcs/cmds/export_cmd.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 09:13:46 by saray             #+#    #+#             */
-/*   Updated: 2022/01/25 23:22:30 by scarboni         ###   ########.fr       */
+/*   Updated: 2022/01/27 15:22:31 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,39 +47,47 @@ static void	strings_init(char *strings[MAX_STRINGS])
 	}
 }
 
+static int	prepare_var_elements(t_env *env, const char **args,
+	char *strings[MAX_STRINGS], int i)
+{
+	int	k;
+
+	if (produce_name_value((char *)args[i], &strings[STRING_NAME],
+			&strings[STRING_VALUE]) == -EXIT_FAILURE)
+		return (strings_clean(strings, -EXIT_FAILURE, args[i],
+				"not valid identifier"));
+	if (ft_strlen(strings[STRING_NAME]) <= 0)
+		return (strings_clean(strings, -EXIT_FAILURE, args[i],
+				"var name must contain at least 1 character"));
+	k = -1;
+	while (strings[STRING_NAME][++k])
+		if (ft_is_valid_for_env_var_name(strings[STRING_NAME][k]) == 0)
+			return (strings_clean(strings, -EXIT_FAILURE, args[i],
+					"not valid identifier"));
+	strings[STRING_RAW] = ft_strdup(args[i]);
+	if (!strings[STRING_RAW])
+		return (strings_clean(strings, -EXIT_FAILURE, NULL, NULL));
+	if ((ft_strlen(strings[STRING_NAME]) == PATH_LEN)
+		&& (ft_strncmp(strings[STRING_NAME], PATH_STR, PATH_LEN) == 0)
+		&& init_path(env) == -EXIT_FAILURE)
+		return (strings_clean(strings, -EXIT_FAILURE, NULL, NULL));
+	return (EXIT_SUCCESS);
+}
+
 int	export_cmd(t_env *env, const char *cmd, const char **args)
 {
 	int		i;
-	int		k;
 	char	*strings[MAX_STRINGS];
 
 	(void)cmd;
 	if (env->pipex_stack.total_item > 1)
 		return (EXIT_SUCCESS);
 	i = 0;
-	k = -1;
 	strings_init(strings);
 	while (args[++i])
 	{
-		if (produce_name_value((char *)args[i], &strings[STRING_NAME],
-				&strings[STRING_VALUE]) == -EXIT_FAILURE)
-			return (strings_clean(strings, -EXIT_FAILURE, args[i],
-					"not valid identifier"));
-		if (ft_strlen(strings[STRING_NAME]) <= 0)
-			return (strings_clean(strings, -EXIT_FAILURE, args[i],
-					"var name must contain at least 1 character"));
-		k = -1;
-		while (strings[STRING_NAME][++k])
-			if (ft_is_valid_for_env_var_name(strings[STRING_NAME][k]) == 0)
-				return (strings_clean(strings, -EXIT_FAILURE, args[i],
-						"not valid identifier"));
-		strings[STRING_RAW] = ft_strdup(args[i]);
-		if (!strings[STRING_RAW])
-			return (strings_clean(strings, -EXIT_FAILURE, NULL, NULL));
-		if ((ft_strlen(strings[STRING_NAME]) == PATH_LEN)
-			&& (ft_strncmp(strings[STRING_NAME], PATH_STR, PATH_LEN) == 0)
-			&& init_path(env) == -EXIT_FAILURE)
-			return (strings_clean(strings, -EXIT_FAILURE, NULL, NULL));
+		if (prepare_var_elements(env, args, strings, i) != EXIT_SUCCESS)
+			return (1);
 		if (find_and_update_env_var(env, strings[STRING_NAME],
 				strings[STRING_VALUE]) == EXIT_SUCCESS)
 		{
@@ -88,7 +96,7 @@ int	export_cmd(t_env *env, const char *cmd, const char **args)
 		}
 		else if (add_new_env_by_value_name_raw(env, strings[STRING_NAME],
 				strings[STRING_VALUE], strings[STRING_RAW]) == -EXIT_FAILURE)
-			return (strings_clean(strings, -EXIT_FAILURE, NULL, NULL));
+			return (strings_clean(strings, 1, NULL, NULL));
 	}
 	return (EXIT_SUCCESS);
 }
