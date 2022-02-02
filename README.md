20d588e5e77907a93de1a54a69aff42f18bd32fa
Sophie <socarboni@gmail.com>
2022-02-02 15:52:22 +0100

    fixing weird $' case

--- README.md
+++ README.md
@@ -4,3 +4,30 @@ ps -ax | grep minishell
 18818 pts/1    S+     0:00 /usr/bin/valgrind.bin --leak-check=full ./minishell
 18856 pts/0    S+     0:00 grep --color=auto minishell
 user42@salty-VirtualBox:~/Documents$ kill -3 18818
+
+
+export pwett="  ''$pwett "
+echo $pwett
+
+export A='      bonjour je suis splited '
+echo @$A@
+
+export A=a B=b C=c
+env
+unset A  B C
+env
+export "d$e"=ert
+
+export "d"=ert
+export "d$"=ert
+
+echo 1 | cat | echo 2 | cat
+
+1;$?;0;2;1
+
+unset PATH
+export PATH=/home/user42/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+export E=e HO=ho
+
+$E'c'"$HO"
--- srcs/datas/parse_dollar.c
+++ srcs/datas/parse_dollar.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 09:42:12 by saray             #+#    #+#             */
-/*   Updated: 2022/02/01 13:48:32 by scarboni         ###   ########.fr       */
+/*   Updated: 2022/02/02 15:49:57 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,24 @@ t_tmp_parsed *tmp_parsed, t_parse_utils p_utils)
 	int	end_var_name_abs;
 	int	res;
 
-	(void)tmp_parsed;
 	end_var_name_abs = go_to_next_needed_i((*line_handle->line),
 			&ft_is_valid_for_env_var_name, (*p_utils.i) + 1);
 	if (end_var_name_abs == (*p_utils.i) + 1
 		&& (*line_handle->line)[end_var_name_abs] == '?')
 		end_var_name_abs++;
 	if (end_var_name_abs == ((*p_utils.i) + 1))
+	{
+		if (tmp_parsed != NULL && ((*line_handle->line)[((*p_utils.i) + 1)] 
+				== '"' || (*line_handle->line)[((*p_utils.i) + 1)] 
+				== '\''))
+		{
+			ft_strlcpy((*line_handle->line) + ((*p_utils.i)), 
+				(*line_handle->line) + ((*p_utils.i) + 1), 
+				ft_strlen((*line_handle->line) + (*p_utils.i)));
+			(*p_utils.i)--;
+		}
 		return (NOT_REPLACED);
+	}
 	res = replace_in_str_one_var(p_utils.env,
 			(t_line){line_handle->line, p_utils.i},
 			end_var_name_abs - (*p_utils.i) - 1,
