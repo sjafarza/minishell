ae5482d01a1ebaefdd17c537f1bf542cbe84b46e
Sophie CARBONI <socarboni@gmail.com>
2022-02-03 11:07:02 +0100

    fixecho

--- srcs/cmds/echo_cmd.c
+++ srcs/cmds/echo_cmd.c
@@ -6,11 +6,25 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 10:47:48 by sjafarza          #+#    #+#             */
-/*   Updated: 2022/02/01 16:37:55 by scarboni         ###   ########.fr       */
+/*   Updated: 2022/02/03 11:06:42 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+int	is_only_n(const char *l)
+{
+	int	i;
+
+	i = 0;
+	while (l[i])
+	{
+		if (l[i] != 'n')
+			return (false);
+		i++;
+	}
+	return (true);
+}
 
 int	echo_cmd(t_env *env, const char *cmd, const char **args, int silent)
 {
@@ -23,8 +37,8 @@ int	echo_cmd(t_env *env, const char *cmd, const char **args, int silent)
 		return (0);
 	i = 0;
 	is_n = false;
-	while (args[++i] && !ft_strncmp(args[i], "-n", 2)
-		&& ft_strlen(args[i]) == 2)
+	while (args[++i] && !ft_strncmp(args[i], "-", 1)
+		&& is_only_n(args[i] + 1) == true)
 		is_n = true;
 	while (args[i])
 	{
